7c2570de9ee7d51fea31d99c9c07dae08dc2ee7f
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-04-12 17:58:21 +0300

    Footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./components/Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
