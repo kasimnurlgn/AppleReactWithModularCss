f7de45677a5c80e04b93e64a918f9d697dc4da41
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-04-12 17:48:31 +0300

    header section added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
