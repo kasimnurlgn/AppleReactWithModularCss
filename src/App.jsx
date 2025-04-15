98f5013c532ef850fb2a42113634401883be87d8
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-04-15 14:31:55 +0300

    general style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import Header from "./components/Header/Header";
+
+import "./App.css";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Alert from "./components/Alert/Alert";
-import FirstSection from "./components/FirstSection/FirstSection";
-import SecondSection from "./components/SecondSection/SecondSection";
-import ThirdSection from "./components/ThirdSection/ThirdSection";
-import FourthSection from "./components/FourthSection/FourthSection";
 
 function App() {
   return (
     <>
-      {/* <Header />  */}
-      {/* <Alert /> */}
-      {/* <FirstSection /> */}
-      {/* <SecondSection /> */}
-      {/* <ThirdSection /> */}
-      {/* <FourthSection /> */}
-      {/* <Footer /> */}
+      <Header />
     </>
   );
 }
